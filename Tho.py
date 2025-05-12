87305120c3d841ce763e6ef95689b422099c9633
ThojTran <trantho18082005@gmail.com>
2025-05-13 00:03:52 +0700

    Add files via upload

---
+++ Tho.py
@@ -0,0 +1,24 @@
+def one():
+    car_name = "Volvo"
+def two():
+    x = 50
+def three_and_four():
+    x = 5
+    y = 10
+    z = x + y
+    print(z)
+def five():
+    x = y = z = "Orange", "banana", "Cherry"
+def six():
+    text = input("Write something: ")
+    print(type(text))
+def seven():
+    help('keywords')
+if __name__ == '__main__':
+    #one()
+    #two()
+    #three_and_four()
+    #five()
+    #six()
+    seven()
+
